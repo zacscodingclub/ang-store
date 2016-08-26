c93c0d4835640c606ec469cb6c848c06c7374591
zacscodingclub <zbaston@gmail.com>
2016-08-26 07:25:10 -0500

    update app reference in existing views. add templates folder

---
+++ app/templates/templates.js
@@ -0,0 +1,12 @@
+angular
+  .module('storeTemplate.templates', ['ngRoute'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/templates', {
+        templateUrl: 'templates/templates.html',
+        controller: 'TemplatesCtrl'
+      })
+  }])
+  .controller('TemplatesCtrl', ['$scope', function($scope) {
+    console.log($scope);
+  }])
