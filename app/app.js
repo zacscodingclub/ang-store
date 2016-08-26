8487d1ec39ffe78c3b91fcafc0db37098d0a575a
zacscodingclub <zbaston@gmail.com>
2016-08-26 08:06:57 -0500

    fix routes by removing unnecessary

--- app/app.js
+++ app/app.js
@@ -1,14 +1,11 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
 angular.module('storeTemplate', [
   'ngRoute',
   'storeTemplate.view1',
   'storeTemplate.view2',
   'storeTemplate.templates'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
+config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/templates'});
 }]);
