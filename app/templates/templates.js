0c6bfa5cae5be6e89fda2fb7a89e234324e765ca
zacscodingclub <zbaston@gmail.com>
2016-08-26 08:07:27 -0500

    rename templates-details to template-details for logical consistency

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -1,12 +1,19 @@
 angular
   .module('storeTemplate.templates', ['ngRoute'])
   .config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-      .when('/templates', {
+    $routeProvider.
+      when('/templates', {
         templateUrl: 'templates/templates.html',
         controller: 'TemplatesCtrl'
+      }).
+      when('/templates/:templateId', {
+        templateUrl: 'templates/template-details.html',
+        controller: 'TemplateDetailsCtrl'
       })
   }])
   .controller('TemplatesCtrl', ['$scope', function($scope) {
+    console.log($scope);
+  }])
+  .controller('TemplateDetailsCtrl', ['$scope', function($scope) {
     console.log($scope);
   }])
